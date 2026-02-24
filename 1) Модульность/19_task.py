99b9df58d6d936ea4f1d2840175357e6e5ad53ff
Daniil Gringruz <dgrin0414@gmail.com>
2026-02-24 17:16:39 +0300

    update task

--- 1) Модульность/19_task.py
+++ 1) Модульность/19_task.py
@@ -3,3 +3,6 @@ from password_generator import generate_password, check_password
 password = generate_password(16)
 
 strength = check_password(password)
+
+print(f"Пароль: {password}")
+print(f"Надежность: {strength}")
